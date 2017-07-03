28863efd93caf03a629d1cfdb1ea99a6404ad8bb
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-07-03 21:30:15 +0400

    First App

---
+++ FirstApp/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use('/', function (req, res){
+  console.log(req.path);
+  res.send("First App....");
+});
+
+app.listen(3000);
+console.log('Server running at http://localhost:3000/');
+
+// return the application object namely app
+module.exports = app;
